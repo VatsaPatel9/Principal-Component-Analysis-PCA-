cba1fe014f601041ed5a79a7892b8bd09080328e
Vatsa Patel <63264063+VatsaPatel9@users.noreply.github.com>
2021-12-11 17:56:02 -0500

    Update average-of-faces.m

--- average-of-faces.m
+++ average-of-faces.m
@@ -5,7 +5,7 @@ clc;
 sum_image = zeros(512,512,3);
 
 for i = 1:132
-    im = imread(['./faces/faces/face' num2str(i) '.png']);
+    im = imread(['./faces_pca/faces_pca/face' num2str(i) '.png']);
     im = imresize(im,[512,512]);
     sum_image = sum_image + double(im);
 end
